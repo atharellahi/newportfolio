56231897e39dcddf017627c0bac535dd27b0d878
Athar Ellahi <official.atharellahi@gmail.com>
2024-01-14 21:06:38 +0530

    navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import styles from './navbar.module.css'
+
+const Navbar = () => {
+
+    return (
+        <>
+            <div className={styles.navbar}>
+                <div className={styles.logoholder}>
+                    <img className={styles.logo} src='/logo/logo.svg'/>
+                </div>
+                <div className={styles.sectiontoggles}>
+                    <div className={styles.section}>Stack</div>
+                    <div className={styles.section}>Projects</div>
+                    <div className={styles.section}>Services</div>
+                    <div className={styles.section}>About</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar;
